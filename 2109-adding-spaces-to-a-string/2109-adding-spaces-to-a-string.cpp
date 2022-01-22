8b6a9acce152662bb8f657ea18d39d83d6846d74
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-01-22 11:37:15 +0530

    Time: 199 ms (38.62%), Space: 81.4 MB (28.96%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
@@ -3,16 +3,23 @@ public:
     string addSpaces(string s, vector<int>& sp) {
         
         string res="";
+        
         int k=0,n=sp.size();
-      
+        
+        
+      if(n==0){return s;}
+        
         for(int i=0;i<s.size();i++)
         {
             if(k<n&&i==sp[k]){res.push_back(' ');
+                              
              k++;
             }
+            
             res.push_back(s[i]);
          
         }
+        
         
     return res;
     }
