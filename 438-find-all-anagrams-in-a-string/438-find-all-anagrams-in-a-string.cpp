32c500bd8893ca30a2bc6e0a5a0755855c58824e
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-02-02 14:10:13 +0530

    Time: 1065 ms (5.01%), Space: 284.9 MB (5.16%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int n,n1;
+
+    bool match(int a[],int b[],string p)
+    {
+        for(int i=0;i<n1;i++)
+        {  
+            if(b[p[i]-'a']!=a[p[i]-'a']||(b[p[i]-'a']==0&&a[p[i]-'a']==0)){return false;}
+        }
+        return true;
+    }
+    vector<int> findAnagrams(string s, string p) {
+        int a[26]={0},b[26]={0};
+        vector<int>v;
+        n=s.size(),n1=p.size();
+        if(n1>n){return v;}
+        
+        for(int i=0;i<n1;i++)
+        {
+           b[p[i]-'a']++;
+        }
+        
+        int i=0,j=0;
+        while(i<n)
+        {
+           
+            a[s[i]-'a']++;
+            if((i-j)+1==n1&&match(a,b, p))
+            {v.push_back(j);
+             }
+            if((i-j)+1==n1)
+            {a[s[j]-'a']--;                           
+            j++;}
+            
+            
+            
+            i++;
+        }
+        return v;
+    }
+};
