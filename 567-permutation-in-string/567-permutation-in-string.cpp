0e549ae13d7268dafc108c1e1c33c08934d86f74
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-02-11 12:10:42 +0530

    Time: 3 ms (98.40%), Space: 7.3 MB (71.57%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool mat(int a[],int b[])
+    {
+        for(int i=0;i<26;i++)
+        {
+            if(a[i]!=b[i]){return false;}
+        }
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+      
+        
+        int a[26]={0},b[26]={0};
+        int n=s1.size(),n2=s2.size();
+    if(n>n2){return false;}
+        for(int i=0;i<n;i++)
+        {a[s1[i]-'a']++;b[s2[i]-'a']++;}
+       for(int i=0;i<n2-n;i++)
+       {
+           if(mat(a,b)){return true;}
+           b[s2[i]-'a']--;
+           b[s2[i+n]-'a']++;
+       }
+         
+        
+        return mat(a,b);
+    }
+};
