ceb39a502384ca9e5e3c50e3fbd892506fdaf95f
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-02-13 15:47:23 +0530

    Time: 4 ms (57.43%), Space: 7.2 MB (67.91%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        
+        for(int i=0;i<(1<<nums.size());i++)
+        { 
+            vector<int>v;
+            for(int j=0;j<nums.size();j++)
+            {
+                if(i&(1<<j))
+                {
+                    
+                    v.push_back(nums[j]);
+                }
+            }
+           
+            res.push_back(v);
+        }
+        return res;
+    }
+};
