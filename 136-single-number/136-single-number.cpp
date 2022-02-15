d82171af6ca74a7519b5525da8115a852fc109e7
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-02-15 14:06:51 +0530

    Time: 20 ms (76.93%), Space: 16.9 MB (85.89%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int res=nums[0];
-        for(int i=1;i<nums.size();i++){res^=nums[i];
+        for(int i=1;i<nums.size();i++){res=res^nums[i];
                                       
         }
         return res;
