f4c2c557fed0f06eeda224f9484baf243a4bc8d6
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-03-01 10:03:52 +0530

    Time: 7 ms (62.32%), Space: 7.9 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-    vector<int> countBits(int n) {
-        vector<int>v;
-        for(int i=0;i<=n;i++)
-        {
-            int ans=0,p=i;
-            while(p)
-        {
-            ans+=p&1;
-            p>>=1;
-            
-        }
-            v.push_back(ans);
-        }
-        
-        return v;
+   vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        ans[0] = 0;
+        for(int i = 1; i<=n; i++)
+            ans[i] = ans[i/2] + i%2;
+        return ans;
     }
 };
