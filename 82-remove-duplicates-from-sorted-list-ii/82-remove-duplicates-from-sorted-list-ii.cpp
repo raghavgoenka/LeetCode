7d3268dd0537966096f3b857fe3fe2225c636d92
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-03-09 12:20:56 +0530

    Time: 8 ms (76.52%), Space: 11.1 MB (59.49%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -14,24 +14,22 @@ public:
         ListNode* curr=head,*prev=new ListNode(2);
         ListNode* t=prev;
         if(head==NULL||head->next==NULL){return head;}
-        while(curr!=NULL&&curr->next!=NULL)
+        while(curr&&curr->next)
         {
-            cout<<curr->val<<" ";
-            if(curr->val!=curr->next->val)
+          if(curr->val!=curr->next->val)
             {
                 prev->next=curr;
                 prev=prev->next;
             }
             else{
            
-                while(curr->next!=NULL&&curr->val==curr->next->val)
+                while(curr->next&&curr->val==curr->next->val)
             {
                 curr=curr->next;
             }
-                
-                prev->next=curr->next?curr->next:NULL;
+            prev->next=curr->next?curr->next:NULL;
             }
-            curr=curr->next;
+           curr=curr->next;
             
         }
         return t->next;
