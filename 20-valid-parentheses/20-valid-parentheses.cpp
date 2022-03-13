40897616f8a4ddb425ee3d904ab98814bd62be49
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-03-13 12:23:26 +0530

    Time: 8 ms (6.87%), Space: 6.4 MB (10.36%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto i:s)
+        {
+            if(i=='('||i=='{'||i=='['){st.push(i);}
+            
+            else if(!st.empty()&&(i==')'||i=='}'||i==']'))
+            {
+                
+                char p=st.top();
+                cout<<p<<" ";
+                if(i==')'&&p!='('){return false;}
+                else if(i=='}'&&p!='{'){return false;}
+                else if(i==']'&&p!='['){return false;}
+                
+                st.pop();
+
+            }
+            else{return false;}
+        }
+        return st.empty()?true:false;;
+    }
+};
