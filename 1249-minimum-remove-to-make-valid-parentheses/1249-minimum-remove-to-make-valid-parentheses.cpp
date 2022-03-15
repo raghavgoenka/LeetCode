7ed61a10ce1ef7053b9021ffab9132997dd46c96
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-03-15 15:06:42 +0530

    Time: 37 ms (39.94%), Space: 11.7 MB (42.01%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+
+
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string str) {
+        stack<int>s; string res="";
+        
+        for(int i=0;i<str.size();i++)
+        {
+            if(str[i]=='('){s.push(i);}
+            else if(str[i]==')')
+            {
+                if(!s.empty()&&str[s.top()]=='('){s.pop();}
+                else{str[i]='1';}
+                 
+            }
+        }
+        while(!s.empty())
+        {
+            str[s.top()]='1';
+            s.pop();
+        }
+        
+        for(auto i:str)
+        {
+            if(i!='1'){res+=i;}
+        }
+       return res;
+        
+    }
+};
