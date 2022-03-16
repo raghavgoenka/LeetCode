16667f9b0156b67769fe393a67d7e5da25355dd3
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-03-16 11:08:15 +0530

    Time: 10 ms (70.16%), Space: 15.4 MB (15.68%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -4,21 +4,22 @@ public:
         int j=0,i=0;
         int m=pop.size(),n=push.size();
         stack<int>s;
-        s.push(push[i]);
-        i++;
+        // s.push(push[i]);
+        // i++;
         while(i<n)
         {   
+            if(s.empty()){s.push(push[i]);i++;}
             while(!s.empty()&&s.top()!=pop[j]&&i<n)
             {
                 s.push(push[i]);
                 i++;
             }
-            if(s.empty()){s.push(push[i]);i++;}
+            
             
             while((!s.empty()&&s.top()==pop[j])&&j<m){s.pop();j++;}
             
         }
-    while((!s.empty()&&s.top()==pop[j]&&j<m)){s.pop();j++;}
+      // while((!s.empty()&&s.top()==pop[j]&&j<m)){s.pop();j++;}
         
         return s.empty();
         
