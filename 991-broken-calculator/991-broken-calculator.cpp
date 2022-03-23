3ff8a31f253de95dfa73517fcc70d5f6b880b8d1
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-03-23 12:25:47 +0530

    Time: 7 ms (9.90%), Space: 5.9 MB (73.18%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int brokenCalc(int s, int t) {
+        if(s==t){return 0;}
+        int c=0;
+        int n=ceil(double(t)/2.0);
+        int res=n;
+       
+    
+        while(n>s){
+          if(n%2!=0){c++;n+=1;continue;}
+            n=ceil(double(n)/2.0);
+           }
+        c+=abs(s-n);
+    cout<<n<<" ";
+        cout<<c<<" ";
+        while(n<t){        
+            n=n*2;c++;}
+        cout<<n<<" ";
+if(n%2==0&&t%2!=0){c++;}
+        if(s>t){return min(c,s-t);}
+        return c;
+        
+       
+    }
+};
