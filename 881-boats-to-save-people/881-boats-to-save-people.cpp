c2617f7f2cf114c02098f5cac42df95988a1352d
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-03-24 10:43:52 +0530

    Time: 151 ms (17.31%), Space: 41.9 MB (57.42%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -6,9 +6,9 @@ public:
          int i=0,j=p.size()-1,c=0;
         while(i<=j)
         {
-            if(i==j){c++;p[i]=0;i++;j--;}
-            else if(p[i]+p[j]>l){p[j]=INT_MAX;j--;c++;}
-            else if(p[i]+p[j]<=l){p[i]=0;p[j]=INT_MAX;i++;j--;c++;}
+            if(i==j){c++;i++;j--;}
+            else if(p[i]+p[j]>l){j--;c++;}
+            else if(p[i]+p[j]<=l){i++;j--;c++;}
             
         }
         return c;
