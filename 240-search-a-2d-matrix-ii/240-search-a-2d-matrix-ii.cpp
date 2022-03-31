7f6b5407c4ad394a3312ed6d698023851522b037
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-03-31 17:56:40 +0530

    Time: 367 ms (12.88%), Space: 15 MB (22.26%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -3,12 +3,19 @@ public:
     bool searchMatrix(vector<vector<int>>& mat, int t) {
         int n=mat.size();
         int m=mat[0].size();
-        int i=0,j=m-1;
-        while(i<n&&j>=0)
+        for(int i=0;i<n;i++)
         {
-            if(mat[i][j]==t){return true;}
-            else if(mat[i][j]>t){j--;}
-            else{i++;}
+            if(t>=mat[i][0]&&t<=mat[i][m-1])
+            {
+                int j=0,k=m-1;
+                while(j<=k)
+                {
+                    int mid=j+(k-j)/2;
+                    if(mat[i][mid]==t){return true;}
+                    else if(mat[i][mid]>t){k=mid-1;}
+                    else {j=mid+1;}
+                }
+            }
         }
         return false;
     }
