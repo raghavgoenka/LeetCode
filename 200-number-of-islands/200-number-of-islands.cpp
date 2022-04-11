f352535877a7572f60928b0c0021c9b3f2a95f5c
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-04-12 00:24:21 +0530

    Time: 104 ms (8.83%), Space: 19.5 MB (9.91%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+   
      void dfs(vector<vector<char>>& M, int n, int m, int i, int j){
         M[i][j] = 'x';
         vector<int>row = {i + 1, i - 1, i, i};
@@ -7,9 +8,12 @@ public:
         for(int k = 0; k < 4; k++){
             if(row[k] < n && col[k] < m && row[k] >= 0 && col[k] >= 0 
                 && M[row[k]][col[k]] == '1'){
+              
                 dfs(M, n, m, row[k], col[k]);
             }
         }
+         
+         
     }
     
     
