66d2d1d4ac41917410ff0a07dfbf2efd8de12769
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-04-28 18:43:00 +0530

    Time: 23 ms (9.68%), Space: 10.3 MB (6.59%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+    
+    vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
+    priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> >pq;
+    
+    pq.push({grid[0][0],{0,0}});
+    int dx[]={0,1};
+    int dy[]={1,0};
+    dis[0][0]=grid[0][0];
+
+         while(!pq.empty())
+         {
+             int w=pq.top().first;
+             int a=pq.top().second.first;
+             int b=pq.top().second.second;
+             pq.pop();
+             for(int i=0;i<2;i++)
+             {
+                 int x=a+dx[i];
+                 int y=b+dy[i];
+                 if(x>=0&&x<n&&y>=0&&y<m&&(w+grid[x][y])<dis[x][y])
+                 {
+                     dis[x][y]=min(dis[x][y],w+grid[x][y]);
+                    //  vis[x][y]=1;
+                    
+                    pq.push({dis[x][y],{x,y}});
+                 }
+             }
+         }
+        //  for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<dis[i][j]<<" ";}cout<<endl;}
+         return dis[n-1][m-1];
+    }
+};
