5ea0dbcd01a9e469c1aeb67fe67fa13aa0c0fde6
Raghav Goenka <57487819+raghavgoenka@users.noreply.github.com>
2022-05-10 15:59:32 +0530

    Time: 2 ms (51.79%), Space: 6.5 MB (58.80%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -5,20 +5,15 @@ public:
     void solve(int k,int n,vector<int>&t,int sum,int p,vector<int>&v)
         
     {
-        
+       
         if(sum==n&&t.size()==k){res.push_back(t);return;}
         else if(sum>n){return ;}
         for(int i=p;i<=9;i++)
-        {   if(v[i]==0){
-              sum+=i;
-            v[i]=1;
+        {   
               t.push_back(i);
-              solve(k,n,t,sum,i,v);
+              solve(k,n,t,sum+i,i+1,v);
               t.pop_back();
-             sum-=i;
-            v[i]=0;
-        }
-            
+             
         }
         return ;
     }
